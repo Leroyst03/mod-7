ac2ad4519c49e63b65784b898a9c6c9a8c472942
Leroyst03 <defaltst@gmail.com>
2025-08-14 18:43:59 +0200

    actualizacion

--- api/v1/docs.js
+++ api/v1/docs.js
@@ -2,11 +2,11 @@ import fs from "fs";
 import path from "path";
 import { serve } from "swagger-ui-dist";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const indexPath = path.join(serve.getAbsoluteFSPath(), "index.html");
   let html = fs.readFileSync(indexPath, "utf8");
 
-  html = html.replace("https://petstore.swagger.io/v2/swagger.json", "/api/v1/spec");
+  html = html.replace("https://petstore.swagger.io/v2/swagger.json", "/api.yaml");
 
   res.setHeader("Content-Type", "text/html");
   res.status(200).send(html);
