e5875a2cda9c2c79e14d9f0f1b0cd130242987dd
Leroyst03 <defaltst@gmail.com>
2025-08-14 18:35:05 +0200

    actualizacion

--- api/v1/docs.js
+++ api/v1/docs.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "openapi.yaml");
+  const filePath = path.join(process.cwd(), "api.yaml");
   const yaml = fs.readFileSync(filePath, "utf8");
 
   res.setHeader("Content-Type", "text/yaml");
