31328824717a25feffa1b36100102a4cd6c2bdf2
Leroyst03 <defaltst@gmail.com>
2025-08-14 18:33:53 +0200

    actualizacion

---
+++ api/v1/docs.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "openapi.yaml");
+  const yaml = fs.readFileSync(filePath, "utf8");
+
+  res.setHeader("Content-Type", "text/yaml");
+  res.status(200).send(yaml);
+}
